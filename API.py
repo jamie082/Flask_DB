6379d253f0ddcc6e600025b25e5ea08f13c09bdc
jamie082 <jamiemorrissey02@gmail.com>
2024-09-05 19:04:14 -0700

    added files to repo

--- API.py
+++ API.py
@@ -1,3 +1,7 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+import json
 # If the DB doesn't exit recreate it else don't overwrite it
 if not os.path.exists('books.db'):
     with app.app_context():
@@ -13,3 +17,21 @@ def add_book():
         serialized_books = [book.as_json for book in books]
         # sending all books back to the client in JSON obkect
         return jsonify(serialized_books)
+    
+    if request.method == "POST":
+        payload = request.get_json()
+        book = Book(isbn=payload['isbn'],
+                title=payload['title'],
+                author=payload['author'])
+    
+        # add new book object to DB
+        db.session.add(book)
+
+        # commit db to save changes
+        db.session.commit()
+
+        # return the same book to let the user know that it has been added to the DB
+        return jsonify(json.dumps(payload))
+
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=5000, debug=True)
--- main.py
+++ main.py
@@ -3,3 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import json
 app = Flask(__name__)
+# definitng URL or Path to save our DB
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://books.db"
+# Creating DB instance to reference throughout the code
+db = SQLAlchemy(app)
+
+# The following code block is used to create a db instance from flask_sqlalchemy and
+# DEfine a model class
+class Book(db.model):
+    isbn = db.Column(db.String(150, primary_key=True)
+    title = db.Column(db.String(150))
+    author = db.Column(db.String(150))
+
+    @property
+    def as_json(self):
+        """ Returns object data in a serializable format
+        """
+        return {
+            'isbn' self.isbn,
+            'title': self.title,
+            'author': self.author
+        }
